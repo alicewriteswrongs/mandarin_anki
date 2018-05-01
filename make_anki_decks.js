0c6e2972cc56922c03f09da9fe9c0708835eb787
Alice Pote <alice.writes.wrongs@gmail.com>
2018-05-01 17:19:24 -0400

    uh ok

--- make_anki_decks.js
+++ make_anki_decks.js
@@ -3,26 +3,34 @@ const pinyin = require("pinyin")
 const program = require("commander")
 const fs = require("fs")
 const { stringify } = require("csv")
-const R = require('ramda')
-const hanzi = require('@zurawiki/hanzi')
+const R = require("ramda")
+const hanzi = require("@zurawiki/hanzi")
 
 const dict = loadSimplified("./cedict_ts.u8")
 
 // First we want to load the Hanzi, ordered by a distributed
 // weighting algorithm based on radical components and frequency.
-  // This order is taken from here: http://learnm.org/indexE.php
-  const hanziDistributedWeightOrder = String(
-    fs.readFileSync("./data/DNWorder.txt")
-  )
+// This order is taken from here: http://learnm.org/indexE.php
+const hanziDistributedWeightOrder = String(
+  fs.readFileSync("./data/DNWorder.txt")
+)
   .split("\n")
   .map(row => row.split(",")[0])
 
 // We're going to organize the decks into 'levels' of 20 hanzi per level
 const levels = R.splitEvery(20, hanziDistributedWeightOrder)
 
-const allRadicals = new Set
-const radicalsByLevel = levels.map(level => {
-  const radicals = level.map(char => hanzi.decompose(char, 2)).reduce((acc, result) => acc.concat(result.components), [])
-})
+// we want to calculate the radicals which are unique to each level
+// so we can group them with the hanzi that contain them
+let allRadicals = []
 
-const testHanzi = first3000Hanzi[0]
+const radicalsByLevel = levels.map(level => {
+  // we only want the radicals which are new in this level
+  const uniqueRadicals = R.compose(
+    R.difference(R.__, allRadicals),
+    R.chain(char => hanzi.decompose(char, 2).components)
+  )(level)
+
+  allRadicals = allRadicals.concat(uniqueRadicals)
+  return uniqueRadicals
+})
